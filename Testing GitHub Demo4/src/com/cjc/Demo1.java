a791c6fe70c915cbe2cd795ee17ee713412af2a1
user <user@LAPTOP-QOF44QE3>
2021-06-11 19:53:46 +0530

    commit by suraj for demo 4

---
+++ Testing GitHub Demo4/src/com/cjc/Demo1.java
@@ -0,0 +1,12 @@
+package com.cjc;
+
+public class Demo1 {
+	public static void m1(String s)
+	{
+		System.out.println(s);
+	}
+public static void main(String[] args) {
+	
+	m1("Surajkkjhbjn");
+}
+}
